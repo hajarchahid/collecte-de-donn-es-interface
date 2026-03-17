0f40aeceac4a8034395399bebcf90d5c754a7191
hajarchahid <hajarchhahid@gmail.com>
2026-03-17 09:58:38 +0000

    ok

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,6 +7,7 @@ export default defineConfig({
     server: {
         host: true,
         port: 5173,
+        allowedHosts: ['www.speechai.fsac.ac.ma', 'speechai.fsac.ac.ma'],
         watch: {
             usePolling: true
         }
